357e6ae7661d787b8a10e980a5c9e6fc84898660
fschweikhardt <schweikhardt.web@gmail.com>
2024-04-23 10:14:50 -0400

    Initial commit with Vite & Tailwind

---
+++ USER_STORIES.md
@@ -0,0 +1,20 @@
+    // swipe down is next
+    // swipe up is previous
+    // swipe right is add to cart (hold in hand)
+    // swipe left is view cart (view what is in hands)
+
+    // cool loading feature 
+    // transitions
+
+    // features ----> 
+    // bargin bin (based on price)
+    // genre ?? so far does not seem possible due to lack of data
+    // search for format type
+
+    // local storage for:
+    // items in cart
+
+    // to dos ----> 
+    // deploy to vercel
+    // update app to react 18 requirements
+    // connect oAuth
---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+interface Record {
+  release: {
+    artist: string;
+    title: string;
+    format: string;
+    images: { resource_url: string }[];
+  };
+  price: {
+    value: string;
+  };
+}
+
+function App() {
+    const [ loaded, setLoaded ] = useState(false)
+    const [ records, setRecords ] = useState<Record[]>([])
+    const [ counter, setCounter ] = useState(0)
+    const [ flipMode, setFlipMode ] = useState(true)
+    const [ menuDisplay, setMenuDisplay ] = useState(false)
+    
+    useEffect(()=> {
+        const getRandomPage = Math.floor(Math.random() * 39)
+        const pageination = `page=${getRandomPage}&per_page=100`
+        const token = `token=${import.meta.env.VITE_API_KEY}`
+        const url = `https://api.discogs.com/users/landlockedmusic/inventory?${pageination}&${token}`
+        const options = {
+          method: 'GET',
+          headers: {
+              'User-Agent': 'LocalRecordStoreApp/1.0 localhost:3000'
+          }
+      };
+        let resFiltered = []
+        fetch(url, options)
+            .then(res => res.json())
+            .then(res => {
+                resFiltered = res.listings.filter( (listing:any) => 
+                    listing.release.artist &&
+                    listing.release.title &&
+                    listing.release.description &&
+                    listing.release.format &&
+                    listing.price.value &&
+                    listing.release.thumbnail && 
+                    listing.release.images[0].resource_url && 
+                    !listing.release.format.match(/CD/) &&
+                    !listing.release.format.match(/Cass/)
+                )
+                setRecords(resFiltered)
+                setLoaded(true)
+            })
+    }, [])
+    
+    if (!loaded) { 
+      return <></>
+  } else {
+      return (
+          <>
+              {!menuDisplay && 
+                  <div>
+                      <header>
+                          <h1>Landlocked Music</h1>
+                          <h3>Bloomington Indiana</h3>  
+                      </header>
+                      <main>
+                          <button onClick={()=>setFlipMode(!flipMode)}>
+                              flip mode is {flipMode ? 'on' : 'off'}
+                          </button>
+                          <br />
+                          <br />
+                          <div>
+                              { !flipMode &&
+                                  <div
+                                      className='flex flex-col items-center gap-2 border-4 border-solid border-black p-2.5'
+                                  >
+                                      <h1 className='text-xl'>{records[counter].release.artist}</h1>
+                                      <img 
+                                          src={records[counter].release.images[0].resource_url} 
+                                          alt={records[counter].release.artist}
+                                          width={window.innerWidth/1.5}
+                                          height={window.innerWidth/1.5}
+                                      />
+                                      <h2>{records[counter].release.title}</h2>
+                                      <h3>{records[counter].release.format}</h3>
+                                      <h3>${records[counter].price.value}</h3>
+                                  </div>
+                              }
+                              { flipMode && 
+                                  <img 
+                                      src={records[counter].release.images[0].resource_url} 
+                                      alt={records[counter].release.artist}
+                                      width={window.innerWidth - 20}
+                                      height={window.innerWidth - 20}
+                                  />
+                              }
+                                  <div
+                                      className='pt-8'
+                                  >
+                                      <button 
+                                          onClick={counter === 0 ? ()=>{} : ()=>setCounter(counter-1)}
+                                      >
+                                          prev
+                                      </button>
+                                      <button 
+                                          className='px-2.5'
+                                      >
+                                          add to cart
+                                      </button>
+                                      <button 
+                                          onClick={counter === records.length-1 ? ()=>window.location.reload() : ()=>setCounter(counter+1)}
+                                      >
+                                          next
+                                      </button>
+                                  </div>
+                          </div>
+                      </main>
+                  </div>
+              }
+              <div>
+                  <button 
+                      className='absolute top-7 right-7 p-1'
+                      onClick={()=>setMenuDisplay(!menuDisplay)}
+                  >
+                      options
+                  </button>
+                  {menuDisplay && 
+                      <button 
+                          className='ml-5'
+                          onClick={()=>window.location.reload()}
+                      >
+                          shuffle
+                      </button>
+                  }
+              </div>
+          </>
+      )
+  }
+}
+
+export default App
